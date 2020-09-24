cbb6728e1be2dc8a76531aca99b90d6567e4400f
Raparthi Saikiran <reparthi_saikiran@tecnics.com>
2020-09-24 11:52:15 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/filter-textbox.component.ts
@@ -0,0 +1,23 @@
+// import { Component, Input, OnInit } from '@angular/core';
+
+
+
+// @Component({
+//     selector: 'app-filter',
+//     templateUrl: '<input type="text" [(ngModel)]="filter">'
+// })
+
+
+// export class FilterComponent implements OnInit {
+//     private _filter:string;
+//     @Input() get filter(){
+//         return this._filter;
+//     }
+//     set filter(val:string){
+
+//     }
+
+//     constructor() { }
+//     ngOnInit(): void { }
+
+// }
